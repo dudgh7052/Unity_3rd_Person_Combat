3851f1d8b86c50fd3cd922bd4451ab5e4f32574c
dudgh7052 <wkd7052@gmail.com>
2024-02-13 15:04:08 +0900

    Create Action Map & InputReader

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, Controls.IPlayerActions
+{
+    public event Action m_jumpEvent;
+    public event Action m_dodgeEvent;
+
+    private Controls m_controls;
+
+    void Start()
+    {
+        m_controls = new Controls();
+        m_controls.Player.SetCallbacks(this);
+
+        m_controls.Player.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        m_controls.Player.Disable();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.performed) { return; }
+
+        m_jumpEvent?.Invoke();
+    }
+
+    public void OnDodge(InputAction.CallbackContext context)
+    {
+        if (!context.performed) { return; }
+
+        m_dodgeEvent?.Invoke();
+    }
+}
